e6b1f21f9bfa46d648e0205e2c0c9ccdf46900bc
tigerroarm <u201613443@hust.edu.cn>
2017-09-03 22:21:47 +0800

    版权说明

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-	<meta charset="utf-8"> 
+	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/my.css">
@@ -29,12 +29,12 @@
 <div class="container">
 	<img src="img/logo.jpg" class="img" >
 	<h3>Sign in to EIC,HUST</h3>
-	<form class="form-horizontal col-sm-5" id = "form-main" onsubmit="return Checked();" action="login/action.php" method="post" name="myform" >
+	<form class="form-horizontal col-sm-4" id = "form-main" onsubmit="return Checked();" action="login/action.php" method="post" name="myform" >
   		<div class="form-group">
     		<label for="studentid">学号：</label>
     		<input type="text" class="form-control" id="studentid" placeholder="学号：U2015×××××" name="studentid" maxlength="10">
   		</div>
- 
+
 	  	<div class="form-group">
 		    <label for="password">密码</label>
 		    <input type="password" class="form-control" id="password" placeholder="请输入您的密码" name="password" maxlength="20">
@@ -42,7 +42,7 @@
 
 		<div class="form-group">
 			<label for="check">验证码</label>
-			<a href="index.php"><img src = 'identify/img.php' /></a>
+			<a href="index.php"><img src = 'identify/img.php' class="check" /></a>
 			<input type="text" name="image" class="form-control" id="image" placeholder="输入图中验证码">
 		</div>
 	<button type="submit" class="btn btn-info">登录</button>
@@ -50,10 +50,9 @@
 	<br />
 	<p>首次登录，请先去<a href="signup.php">注册</a></p>
 	</form>
+
+	<p class="footer">2017 © Powered by 科协技术部  All rights reserved.</p>
 </div>
-
-
-
 
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-	<meta charset="utf-8"> 
+	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/my.css">
@@ -12,12 +12,12 @@
 	<div class="container">
 	<img src="img/logo.jpg" class="img" >
 	<h3>Sign up to EIC,HUST</h3>
-	<form class="form-horizontal col-sm-5" id="form-main" action="login/register.php" method="post">
+	<form class="form-horizontal col-sm-4" id="form-main" action="login/register.php" method="post">
   		<div class="form-group">
     		<label for="studentid">学号：</label>
     		<input type="text" class="form-control" id="studentid" placeholder="学号：U2015×××××" name="studentid" maxlength="10">
   		</div>
-  
+
 
 	  	<div class="form-group">
 		    <label for="password">密码：</label>
@@ -31,12 +31,14 @@
 
 		<div class="form-group">
 			<label for="check">验证码</label>
-			<a href="signup.php"><img src="identify/img.php"></a>
+			<a href="signup.php"><img src="identify/img.php" class="check"></a>
 			<input type="text" name="image" class="form-control" id="image" placeholder="输入图中验证码">
 		</div>
 	<button type="submit" class="btn btn-info" name="signup">提交注册</button>
 	<button type="reset" class="btn btn-info">重置</button>
 	</form>
+
+	<p class="footer">2017 © Powered by 科协技术部  All rights reserved.</p>
 </div>
 
 
